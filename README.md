14a6006fe64eceddf02d26a076348f2a901fc94d
Samaila-Tsingiri <sumzybadoo@gmail.com>
2023-10-02 20:48:08 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# edoc-echanneling
+
+  1.Admin
+  
+  
+    Admin can add doctors,edit doctors, delete doctors;
+    
+    Schedule new doctors sessions,remove sessions;
+    
+    View patients details;
+    
+    View booking of patients;
+    
+    
+
+ 
+  2.Doctors
+  
+  
+    View their Appointment;
+    
+    view their scheduled sessions;
+    
+    view details of patients;
+    
+    delete account;
+    
+    edit account settings;
+    
+    
+  3.Patiens(Clients)
+  
+  
+    make appointment online;
+    
+    create accounts themslves;
+    
+    view their old booking;
+    
+    delete account;
+    
+    edit account settings;
+    
+    
+ 
+If you are Admin,doctor or patient ,only have one page to login :)
+
+  
+-----------------------------------------------
+
+
+
+
+
+
+
+
+DATABASE NAME: 'habs'
+
+# BUILDIN USER ACCOUNTS OF THIS PROJECT
+
+ADMIN EMAIL:		admin@LiveDoc.com
+
+ADMNIN PASSWORD:	123
+
+
+DOCTOR EMAIL:		doctor@LiveDoc.com
+
+DOCTOR PASSWORD:	123
+
+
+PATIENT EMAIL:		patient@LiveDoc.com
+
+PATIENT PASSWORD:	123
+
+
+
+
+
+
---
+++ create-account.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/animations.css">  
+    <link rel="stylesheet" href="css/main.css">  
+    <link rel="stylesheet" href="css/signup.css">
+        
+    <title>Create Account</title>
+    <style>
+        .container{
+            animation: transitionIn-X 0.5s;
+        }
+    </style>
+</head>
+<body>
+<?php
+
+//learn from w3schools.com
+//Unset all the server side variables
+
+session_start();
+
+$_SESSION["user"]="";
+$_SESSION["usertype"]="";
+
+// Set the new timezone
+date_default_timezone_set('Asia/Kolkata');
+$date = date('Y-m-d');
+
+$_SESSION["date"]=$date;
+
+
+//import database
+include("connection.php");
+
+
+
+
+
+if($_POST){
+
+    $result= $database->query("select * from webuser");
+
+    $fname=$_SESSION['personal']['fname'];
+    if(!preg_match("/^[a-zA-Z]+$/",$fname)) { die ("Invalid Name");}
+    $lname=$_SESSION['personal']['lname'];
+    if(!preg_match("/^[a-zA-Z]+$/",$fname)) { die ("Invalid Name");}
+    $name=$fname." ".$lname;
+    $address=$_SESSION['personal']['address'];
+    if(!preg_match("/^[a-zA-Z\-\ ]+$/",$address)) { die ("Invalid Address");}
+    $nic=$_SESSION['personal']['nic'];
+    if(!preg_match("/^[0-9]+$/",$nic)) { die ("Invalid NIN");}
+    $dob=$_SESSION['personal']['dob'];
+    $email=$_POST['newemail'];
+    if(!preg_match("/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}$/",$email)) { die ("Invalid Email, First letter must be Capital letter");}
+    $tele=$_POST['tele'];
+    if(!preg_match("/^[0-9]+$/",$tele)) { die ("Invalid Phone Number");}
+    $newpassword=$_POST['newpassword'];
+    $cpassword=$_POST['cpassword'];
+    
+    if ($newpassword==$cpassword){
+        $result= $database->query("select * from webuser where email='$email';");
+        if($result->num_rows==1){
+            $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">Already have an account for this Email address.</label>';
+        }else{
+            
+            $database->query("insert into patient(pemail,pname,ppassword, paddress, pnic,pdob,ptel) values('$email','$name','$newpassword','$address','$nic','$dob','$tele');");
+            $database->query("insert into webuser values('$email','p')");
+
+            //print_r("insert into patient values($pid,'$email','$fname','$lname','$newpassword','$address','$nic','$dob','$tele');");
+            $_SESSION["user"]=$email;
+            $_SESSION["usertype"]="p";
+            $_SESSION["username"]=$fname;
+
+            header('Location: patient/index.php');
+            $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;"></label>';
+        }
+        
+    }else{
+        $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">Password Confirmation Error! Reconfirm Password</label>';
+    }
+
+
+
+    
+}else{
+    //header('location: signup.php');
+    $error='<label for="promter" class="form-label"></label>';
+}
+
+?>
+
+
+    <center>
+    <div class="container">
+        <table border="0" style="width: 69%;">
+            <tr>
+                <td colspan="2">
+                    <img src="img/logo1.png" alt="" height= "50px" style="margin-top: 50px;padding: 0; " >
+                    <p class="header-text">Let's Get Started</p>
+                    <p class="sub-text">It's Okey, Now Create User Account.</p>
+                </td>
+            </tr>
+            <tr>
+                <form action="" method="POST" >
+                <td class="label-td" colspan="2">
+                    <label for="newemail" class="form-label">Email: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="email" name="newemail" class="input-text" placeholder="Email Address" required>
+                </td>
+                
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="tele" class="form-label">Mobile Number: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="tel" name="tele" class="input-text"  placeholder="+234" maxlength="14" pattern="{0}[0-9]{9}" >
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="newpassword" class="form-label">Create New Password: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="password" name="newpassword" class="input-text" placeholder="New Password" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="cpassword" class="form-label">Confirm Password: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="password" name="cpassword" class="input-text" placeholder="Confirm Password" required>
+                </td>
+            </tr>
+     
+            <tr>
+                <td colspan="2">
+                    <?php echo $error ?>
+
+                </td>
+            </tr>
+            
+            <tr>
+                <td>
+                    <input type="reset" value="Reset" class="login-btn btn-primary-soft btn" >
+                </td>
+                <td>
+                    <input type="submit" value="Sign Up" class="login-btn btn-primary btn">
+                </td>
+
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <br>
+                    <label for="" class="sub-text" style="font-weight: 280;">Already have an account&#63; </label>
+                    <a href="login.php" class="hover-link1 non-style-link">Login</a>
+                    <br><br><br>
+                </td>
+            </tr>
+
+                    </form>
+            </tr>
+        </table>
+
+    </div>
+</center>
+</body>
+</html>
